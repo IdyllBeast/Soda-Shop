dea951e1959e80942c8e475b45b0e51f32ea8bbd
Easton Janecek <easton.janecek@gmail.com>
2025-08-27 13:24:47 -0700

    assets update

--- SodaShop.lua
+++ SodaShop.lua
@@ -1958,3 +1958,471 @@ SMODS.Joker({
 		end
 	end,
 })
+
+SMODS.Joker {
+    key = 'slurm',
+    loc_txt = {
+        name = 'Slurm',
+        text = {
+            '{C:mult}+#1#{} Mult#2#',
+            'Only evolves when',
+            '{C:attention}leftmost{} Joker',
+            '{C:inactive}(Stage #3#){}'
+        }
+    },
+    config = {extra = {rounds_held = 0, stage = 1, reset_flag = false}},
+    rarity = 2, -- Uncommon
+    cost = 7,
+    atlas = 'SodaShop',
+    pos = {x = 4, y = 4},
+    blueprint_compat = false, -- Incompatible with blueprint/brainstorm
+    loc_vars = function(self, info_queue, center)
+        local stage = (center and center.ability and center.ability.extra and center.ability.extra.stage) or 1
+        local mult_value = 3
+        local extra_text = ""
+        
+        if stage == 1 then
+            mult_value = 3
+            extra_text = ""
+        elseif stage == 2 then
+            mult_value = 3
+            extra_text = ", {C:money}$1{} per hand"
+        elseif stage == 3 then
+            mult_value = 5
+            extra_text = ", {C:money}$2{} per hand"
+        elseif stage == 4 then
+            mult_value = 8
+            extra_text = ", {C:money}$3{} per hand, {X:mult,C:white}X1.2{} Mult"
+        else -- stage 5+
+            mult_value = 12
+            extra_text = ", {C:money}$5{} per hand, {X:mult,C:white}X1.5{} Mult"
+        end
+        
+        return {vars = {mult_value, extra_text, stage}}
+    end,
+    calculate = function(self, card, context)
+        -- Check if this is the start of a new round
+        if context.setting_blind and not context.blueprint then
+            local is_leftmost = (G.jokers.cards[1] == card)
+            
+            if is_leftmost then
+                -- Increase rounds held
+                card.ability.extra.rounds_held = card.ability.extra.rounds_held + 1
+                card.ability.extra.reset_flag = false
+                
+                -- Check for stage evolution (every 5 rounds)
+                local new_stage = math.min(5, math.floor((card.ability.extra.rounds_held - 1) / 5) + 1)
+                
+                if new_stage > card.ability.extra.stage then
+                    card.ability.extra.stage = new_stage
+                    card_eval_status_text(card, 'extra', nil, nil, nil, {
+                        message = "Slurm Evolved! Stage " .. new_stage,
+                        colour = G.C.FILTER
+                    })
+                end
+            else
+                -- Not leftmost - reset if we haven't already shown the reset message
+                if not card.ability.extra.reset_flag then
+                    card.ability.extra.rounds_held = 0
+                    card.ability.extra.stage = 1
+                    card.ability.extra.reset_flag = true
+                    
+                    card_eval_status_text(card, 'extra', nil, nil, nil, {
+                        message = "Slurm Addiction Reset!",
+                        colour = G.C.RED
+                    })
+                end
+            end
+        end
+        
+        -- Apply effects based on current stage
+        if context.joker_main then
+            local stage = card.ability.extra.stage
+            local ret = {}
+            
+            if stage == 1 then
+                -- Stage 1: +3 Mult
+                return {
+                    message = localize{type='variable',key='a_mult',vars={3}},
+                    mult_mod = 3
+                }
+            elseif stage == 2 then
+                -- Stage 2: +3 Mult, $1 per hand
+                ease_dollars(1)
+                return {
+                    message = localize{type='variable',key='a_mult',vars={3}} .. ", $1",
+                    mult_mod = 3,
+                    dollars = 1
+                }
+            elseif stage == 3 then
+                -- Stage 3: +5 Mult, $2 per hand
+                ease_dollars(2)
+                return {
+                    message = localize{type='variable',key='a_mult',vars={5}} .. ", $2",
+                    mult_mod = 5,
+                    dollars = 2
+                }
+            elseif stage == 4 then
+                -- Stage 4: +8 Mult, $3 per hand, X1.2 Mult
+                ease_dollars(3)
+                return {
+                    message = localize{type='variable',key='a_mult',vars={8}} .. ", $3, X1.2",
+                    mult_mod = 8,
+                    Xmult_mod = 1.2,
+                    dollars = 3
+                }
+            else -- stage 5+
+                -- Stage 5: +12 Mult, $5 per hand, X1.5 Mult
+                ease_dollars(5)
+                return {
+                    message = localize{type='variable',key='a_mult',vars={12}} .. ", $5, X1.5",
+                    mult_mod = 12,
+                    Xmult_mod = 1.5,
+                    dollars = 5
+                }
+            end
+        end
+    end
+}
+
+SMODS.Joker {
+    key = 'unseen_explosion',
+    loc_txt = {
+        name = 'Unseen Explosion',
+        text = {
+            'Copies ability of',
+            '{C:attention}leftmost{} Joker',
+            'Copies ability of', 
+            'Joker to the {C:attention}right{}',
+            '{X:mult,C:white}X2{} Mult'
+        }
+    },
+    config = {extra = {Xmult = 2}},
+    rarity = 3, -- We'll override the shop appearance logic
+    cost = 8,
+    blueprint_compat = true,
+    pos = {x = 0, y = 0}, -- You'll need to set proper sprite coordinates
+    atlas = 'test',
+    calculate = function(self, card, context)
+        local ret = {}
+        
+        -- Apply the X2 mult first
+        if context.joker_main then
+            return {
+                message = localize{type='variable',key='a_xmult',vars={card.ability.extra.Xmult}},
+                Xmult_mod = card.ability.extra.Xmult
+            }
+        end
+        
+        -- Copy leftmost joker ability (Blueprint logic)
+        if context.blueprint then
+            context.blueprint_card = context.blueprint_card or card
+            if context.blueprint_card == card then
+                local leftmost_joker = G.jokers.cards[1] -- Get first joker in array
+                if leftmost_joker and leftmost_joker ~= card then
+                    context.blueprint_card = leftmost_joker
+                    if leftmost_joker.calculate_joker then
+                        local other_ret = leftmost_joker:calculate_joker(context)
+                        if other_ret then 
+                            table.insert(ret, other_ret)
+                        end
+                    end
+                end
+            end
+        end
+        
+        -- Copy right joker ability (Brainstorm logic)  
+        if context.brainstorm then
+            context.brainstorm_card = context.brainstorm_card or card
+            if context.brainstorm_card == card then
+                local other_joker = nil
+                for i = 1, #G.jokers.cards do
+                    if G.jokers.cards[i] == card then
+                        other_joker = G.jokers.cards[i+1]
+                        break
+                    end
+                end
+                if other_joker and other_joker ~= card then
+                    context.brainstorm_card = other_joker
+                    if other_joker.calculate_joker then
+                        local other_ret = other_joker:calculate_joker(context)
+                        if other_ret then
+                            table.insert(ret, other_ret)
+                        end
+                    end
+                end
+            end
+        end
+        
+        if #ret > 0 then return ret end
+    end
+}
+
+-- Override shop generation to implement 1 in 7 chance
+local original_create_card = create_card
+function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    local card = original_create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
+    
+    -- Only affect joker cards in shop
+    if _type == 'Joker' and area == G.shop_jokers then
+        -- 1 in 7 chance for Unseen Explosion to appear in any shop
+        if not G.GAME.unseen_explosion_available then
+            G.GAME.unseen_explosion_available = (pseudorandom('unseen_explosion_spawn') < 1/7)
+        end
+        
+        -- If this run allows Unseen Explosion and we roll to replace a card
+        if G.GAME.unseen_explosion_available and pseudorandom('unseen_explosion_replace') < 0.05 then
+            -- Replace this card with Unseen Explosion
+            local unseen_center = G.P_CENTERS.j_SodaShop_unseen_explosion
+            if unseen_center then
+                card.config.center = unseen_center
+                card:set_ability(unseen_center, nil, nil)
+                G.GAME.unseen_explosion_appeared = true
+            end
+        end
+    end
+    
+    return card
+end
+
+-- Reset the availability flag when starting a new run
+local original_init_game_object = Game.init_game_object
+function Game.init_game_object(self)
+    local game_object = original_init_game_object(self)
+    game_object.unseen_explosion_available = nil
+    game_object.unseen_explosion_appeared = nil
+    return game_object
+end
+
+SMODS.Joker {
+    key = 'canned_strawberry_shortcake',
+    loc_txt = {
+        name = 'Canned Strawberry Shortcake',
+        text = {
+            '{C:attention}Dessert Special:{}',
+            'On {C:attention}final hand{} of blind,',
+            'earn {C:money}$1{} per hand played',
+            'this blind {C:inactive}(Currently: #1#){}'
+        }
+    },
+    config = {extra = {}},
+    rarity = 1, -- Common
+    cost = 5,
+    pos = {x = 0, y = 0}, -- You'll need to set proper sprite coordinates
+    atlas = 'test',
+    loc_vars = function(self, info_queue, center)
+        local hands_played = G.GAME.current_round.hands_played or 0
+        return {vars = {hands_played}}
+    end,
+    calculate = function(self, card, context)
+        -- Check if this is the final hand (blind is about to be defeated)
+        if context.joker_main and context.scoring_hand then
+            local current_chips = to_big(G.GAME.chips)
+            local blind_chips = to_big(G.GAME.blind.chips)
+            
+            -- If this hand will defeat the blind (current chips >= blind requirement)
+            if current_chips >= blind_chips then
+                local hands_played = G.GAME.current_round.hands_played or 0
+                
+                if hands_played > 0 then
+                    -- Give money based on hands played this blind
+                    ease_dollars(hands_played)
+                    
+                    return {
+                        message = "Dessert Special! $" .. hands_played,
+                        dollars = hands_played,
+                        colour = G.C.MONEY
+                    }
+                end
+            end
+        end
+    end
+}
+
+SMODS.Joker {
+    key = 'canned_corn_soup',
+    loc_txt = {
+        name = 'Canned Corn Soup',
+        text = {
+            '{C:mult}+Mult{} based on how',
+            'little {C:money}money{} you have',
+            'More comfort when struggling',
+            'Currently: {C:mult}+#1#{} Mult'
+        }
+    },
+    config = {extra = {}},
+    rarity = 1, -- Common
+    cost = 4,
+    pos = {x = 0, y = 0}, -- You'll need to set proper sprite coordinates
+    atlas = 'test',
+    loc_vars = function(self, info_queue, center)
+        local money = G.GAME.dollars or 0
+        local mult_bonus = 0
+        
+        -- Calculate mult based on money
+        if money <= -10 then
+            mult_bonus = 40
+        elseif money <= -5 then
+            mult_bonus = 30
+        elseif money == 0 then
+            mult_bonus = 20
+        elseif money <= 10 then
+            mult_bonus = 15
+        elseif money <= 20 then
+            mult_bonus = 12
+        elseif money <= 30 then
+            mult_bonus = 8
+        elseif money <= 40 then
+            mult_bonus = 5
+        elseif money <= 50 then
+            mult_bonus = 2
+        else
+            mult_bonus = 1
+        end
+        
+        return {vars = {mult_bonus}}
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            local money = G.GAME.dollars or 0
+            local mult_bonus = 0
+            
+            -- Calculate mult based on current money
+            if money <= -10 then
+                mult_bonus = 40
+            elseif money <= -5 then
+                mult_bonus = 30
+            elseif money == 0 then
+                mult_bonus = 20
+            elseif money <= 10 then
+                mult_bonus = 15
+            elseif money <= 20 then
+                mult_bonus = 12
+            elseif money <= 30 then
+                mult_bonus = 8
+            elseif money <= 40 then
+                mult_bonus = 5
+            elseif money <= 50 then
+                mult_bonus = 2
+            else
+                mult_bonus = 1
+            end
+            
+            if mult_bonus > 0 then
+                return {
+                    message = localize{type='variable',key='a_mult',vars={mult_bonus}},
+                    mult_mod = mult_bonus
+                }
+            end
+        end
+    end
+}
+
+SMODS.Joker {
+    key = 'rerollercoaster_tycoon',
+    loc_txt = {
+        name = 'Rerollercoaster Tycoon',
+        text = {
+            '{C:green}+1{} free reroll per round',
+            'Unused rerolls {C:attention}accumulate{}',
+            'Currently: {C:attention}#1#{} free rerolls'
+        }
+    },
+    config = {extra = {free_rerolls = 0}},
+    rarity = 2, -- Uncommon
+    cost = 6,
+    pos = {x = 0, y = 0}, -- You'll need to set proper sprite coordinates
+    atlas = 'test',
+    loc_vars = function(self, info_queue, center)
+        return {vars = {center.ability.extra.free_rerolls or 0}}
+    end,
+    calculate = function(self, card, context)
+        -- Add a free reroll at the start of each round
+        if context.setting_blind and not context.blueprint then
+            card.ability.extra.free_rerolls = card.ability.extra.free_rerolls + 1
+            card_eval_status_text(card, 'extra', nil, nil, nil, {
+                message = "+1 Free Reroll",
+                colour = G.C.GREEN
+            })
+            return {
+                message = "Free rerolls: " .. card.ability.extra.free_rerolls,
+                colour = G.C.FILTER
+            }
+        end
+    end
+}
+
+-- Override the reroll function to use free rerolls first
+local original_reroll_shop = G.FUNCS.reroll_shop
+G.FUNCS.reroll_shop = function(e)
+    -- Check if we have any Rerollercoaster Tycoon jokers with free rerolls
+    local free_reroll_used = false
+    
+    for i = 1, #G.jokers.cards do
+        local joker = G.jokers.cards[i]
+        if joker.config.center.key == 'j_rerollercoaster_tycoon' and joker.ability.extra.free_rerolls > 0 then
+            -- Use a free reroll
+            joker.ability.extra.free_rerolls = joker.ability.extra.free_rerolls - 1
+            free_reroll_used = true
+            
+            -- Show feedback
+            card_eval_status_text(joker, 'extra', nil, nil, nil, {
+                message = "Free Reroll Used!",
+                colour = G.C.GREEN
+            })
+            
+            break -- Only use one free reroll per click
+        end
+    end
+    
+    -- If we used a free reroll, don't charge money
+    if free_reroll_used then
+        -- Manually trigger the shop reroll without cost
+        local cost = G.GAME.current_round.reroll_cost or 5
+        G.GAME.current_round.reroll_cost_increase = G.GAME.current_round.reroll_cost_increase or 2
+        
+        -- Reroll the shop
+        G.shop_jokers:remove()
+        G.shop_booster:remove()
+        G.shop_vouchers:remove()
+        
+        -- Create new shop items
+        for i = 1, G.shop_jokers.config.card_limit do
+            local card = create_card('Joker', G.shop_jokers, nil, nil, nil, nil, nil, 'sho')
+            create_shop_card_ui(card, 'Joker', G.shop_jokers)
+            card.states.visible = false
+            G.shop_jokers:emplace(card)
+        end
+        
+        for i = 1, G.shop_booster.config.card_limit do
+            local card = create_card('Booster', G.shop_booster, nil, nil, nil, nil, nil, 'sho')
+            create_shop_card_ui(card, 'Booster', G.shop_booster)
+            card.states.visible = false
+            G.shop_booster:emplace(card)
+        end
+        
+        for i = 1, G.shop_vouchers.config.card_limit do 
+            local card = create_card('Voucher', G.shop_vouchers, nil, nil, nil, nil, nil, 'sho')
+            create_shop_card_ui(card, 'Voucher', G.shop_vouchers)
+            card.states.visible = false
+            G.shop_vouchers:emplace(card)
+        end
+        
+        -- Increase the reroll cost for subsequent paid rerolls
+        G.GAME.current_round.reroll_cost = cost + G.GAME.current_round.reroll_cost_increase
+        
+        -- Play reroll sound
+        play_sound('coin2', 1, 0.4)
+        
+        -- Update UI
+        for k, v in pairs(G.I.CARDAREA) do
+            if v.config.type == 'shop' then
+                v.config.card_limit = #v.cards
+            end
+        end
+        
+    else
+        -- No free rerolls available, use original function
+        original_reroll_shop(e)
+    end
+end
